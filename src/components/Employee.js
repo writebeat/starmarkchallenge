a6a90ba5a343a0449aa16d471d5752bb57092656
Alex Alvarez <writebeat@gail.com>
2018-06-14 19:33:27 -0400

    Removing unused variable to get rid of a warning from react.

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { formatPhone, formatDate, detailLine } from './Functions';
+import { formatDate, detailLine } from './Functions';
 import '../style/employee.css';
 
 /**
